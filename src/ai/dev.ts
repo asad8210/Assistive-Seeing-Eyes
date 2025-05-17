038f3f4a3a8d7e21db000e90cd190a82ac44f3b5
Asad Khan <123837152+asad8210@users.noreply.github.com>
2025-05-17 10:14:17 +0530

    dev.ts

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,9 +1,7 @@
-// dotenv config is called in genkit.ts
 
-// Import your flows here to register them with Genkit
+import { config } from 'dotenv';
+config();
+
 import '@/ai/flows/content-relevance.ts';
 import '@/ai/flows/personal-assistant.ts';
 import '@/ai/flows/describe-detailed-scene-flow.ts';
-
-// The Genkit development server will pick up flows defined
-// using ai.defineFlow(...) in the imported files.

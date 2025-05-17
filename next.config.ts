3874827d53abe2f99bfe5e9ad4e8601176174a74
Asad Khan <123837152+asad8210@users.noreply.github.com>
2025-05-17 15:15:17 +0530

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  // Generate static files for Capacitor
+  output: 'export',
+
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+
+  // Allow external placeholder images (remove or extend as you need)
   images: {
     remotePatterns: [
       {
